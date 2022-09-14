bab5d49a780c7d9ef2e7516e79aad4ccea348292
AarthiJuryala <93989962+AarthiJuryala@users.noreply.github.com>
2022-09-14 15:17:45 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ mongoose.connect(url,{useNewUrlParser:true})
 const con = mongoose.connection
 const app = express();
 
+
+const bodyparser=require('body-parser')
+const cors=require('cors')
+
+
+app.use(cors())
+app.use(bodyparser.urlencoded({extended:true}))
+
+
+
 app.use(express.json())
 const r = require('./controller/controller')
 app.use('/student',r)
